d5debf8903b8580d13bb59cc68882ea081353b90
MUBASSHIRAHMED\Mubasshir Ahmed <MubasshirAhmed26@gmail.com>
2018-08-15 15:54:34 +0600

    first commit

---
+++ RaycastTargetLoadingBar/Assets/Scripts/RayCastScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastScript : MonoBehaviour
+{
+    public static string rayColliderName;
+    // Use this for initialization
+    void Start()
+    {
+        rayColliderName = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        {
+            rayColliderName = hit.collider.gameObject.name;
+            Debug.Log(rayColliderName);
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+        }
+        else
+            rayColliderName = null;
+    }
+}
---
+++ RaycastTargetLoadingBar/Assets/Scripts/TargetLodingBarScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetLodingBarScript : MonoBehaviour
+{
+    private Image fillImg;
+    // Use this for initialization
+    void Start()
+    {
+        fillImg = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (RayCastScript.rayColliderName == "Target")
+            fillImg.fillAmount += Time.deltaTime;
+        else
+            fillImg.fillAmount = 0;
+    }
+}
